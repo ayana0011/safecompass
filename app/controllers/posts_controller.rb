6eee0f58ab1fbb09a7e4f6a0f34076f1f19be46d
a f <af@enoMacBook-Pro.local>
2024-03-07 19:41:45 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def show
+    @id= params[:id]
+    @post = Post.find_by(id: params[:id])
+    end
+
+
+
+  
+    def new 
+    end
+
+  def create
+    @post = Post.new(content: params[:content])
+    redirect_to("/posts/index")
+    @post.save
+    end
+
+    def edit
+      @post = Post.find_by(id: params[:id])
+    end
+
+    def update
+      @post = Post.find_by(id: params[:id])
+      @post.content = params[:content]
+      @post.save
+      redirect_to("/posts/index")
+    end
+    
+    def destroy
+      @post = Post.find_by(id: params[:id])
+      @post.destroy
+      redirect_to("/posts/index")
+    end
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+Rails.application.routes.draw do
+  post "logout" => "users#logout"
+  get "logout" => "users#logout"
+  post "login" => "users#login"
+  get "login" => "users#login_form"
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "users/:id" => "users#show"
+  get "signup" => "users#new"
+  post "posts/create" => "posts#create"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "posts/:id/destroy" => "posts#destroy"
+  get "/" => "home#top"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+
+end
